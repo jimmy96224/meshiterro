2fb73d197f4eca2efcb399d3f3d691c89574aa4f
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-09-19 10:40:43 +0000

    [fin]ch9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
   devise_for :users
   root to: 'homes#top'
+  
+  resources :post_images, only: [:new, :index, :show]
+  
   get 'homes/about', to: 'homes#about', as: 'about'
 end
